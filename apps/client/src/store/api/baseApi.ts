e2d484437daaf390c9090b8881740c2e74897bbb
Misha Anisimov <158746523+Anisimov-Misha@users.noreply.github.com>
2026-03-04 21:38:00 +0200

    Update baseApi.ts

--- apps/client/src/store/api/baseApi.ts
+++ apps/client/src/store/api/baseApi.ts
@@ -4,7 +4,7 @@ import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: graphqlRequestBaseQuery({
-    url: 'http://localhost:3000/graphql',
+        url: import.meta.env.VITE_API_URL || 'http://localhost:3000/graphql',
   }),
   tagTypes: ['Form', 'Response'],
   endpoints: () => ({}),
